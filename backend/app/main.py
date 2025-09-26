193a32981fdfc9e8ccc4822d0e3a4f2c066e0ff5
Jihoon Lee <jeehoon2016@naver.com>
2025-09-26 10:34:24 +0900

    fix: main부분 수정

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
-from .routers import user
-from app.routers import db_test
+from app.routers import db_test, auth
 
 app = FastAPI()
 
 # 테이블은 이미 MySQL에 있으므로 create_all() 생략
 
-app.include_router(user.router)
+app.include_router(auth.router)   # 임시 로그인/회원가입 라우터
 app.include_router(db_test.router)
