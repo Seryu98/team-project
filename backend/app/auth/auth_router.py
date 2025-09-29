29e82dc0bce58c7801fdfcaa977d3bad29a96200
Jihoon Lee <jeehoon2016@naver.com>
2025-09-29 13:22:06 +0900

    fix: 폴더구조 변경
    기존 구조가 너무 난잡해서
    기능별 구조로 변경했습니다
    readme문서도 수정완료

--- backend/app/auth/auth_router.py
+++ backend/app/auth/auth_router.py
@@ -1,11 +1,11 @@
-# app/routers/auth.py
+# app/routers/auth_router.py
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.core.deps import get_current_user
-from app.schemas.auth import UserRegister, UserLogin, TokenResponse
-from app.services import auth_service
-from app.models import User
+from app.auth.auth_schema import UserRegister, UserLogin, TokenResponse
+from app.auth import auth_service
+from app.users.user_model import User
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -15,6 +15,7 @@ def register(user: UserRegister, db: Session = Depends(get_db)):
     new_user = auth_service.register_user(db, user)
     return {"success": True, "message": "User registered", "user_id": new_user.id}
 
+# 로그인
 @router.post("/login", response_model=TokenResponse)
 def login(user: UserLogin, db: Session = Depends(get_db)):
     token = auth_service.login_user(db, user)
--- backend/app/test/test.py
+++ backend/app/test/test.py
@@ -1,3 +1,4 @@
+#bcrypt 해시가 잘 생성되는지 테스트” 하기 위해 만든 임시 스크립트
 from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 print(pwd_context.hash("test1234"))
